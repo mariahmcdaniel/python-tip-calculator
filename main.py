a97c0b02989fe81e97da7bf5f6d4de32a9f277a8
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-17 21:02:40 -0400

    formatting

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ bill = float(input("What is the total price of the bill? $"))
 
 percent = int(input("\nWhat percentage of the bill would you like to give for the tip? (10, 12, or 15)\n"))
 
-print(f"percent = {percent}\n")
-print(type(percent))
+# print(f"percent = {percent}\n")
+# print(type(percent))
 
 percentCal = round((percent/100 + 1), 2)
-print(percentCal)
+# print(percentCal)
 
 people = int(input("How many people are splitting the bill?\n"))
 
-print(f"people = {people}\n")
-print(type(people))
+# print(f"people = {people}\n")
+# print(type(people))
 
 total = round(bill * percentCal, 2)
 
-print(f"total = {total}\n")
-print(type(total))
+# print(f"total = {total}\n")
+# print(type(total))
 
 each = round(total / people, 2)
 
-print(f"each = {each}\n")
-print(type(each))
+# print(f"each = {each}\n")
+# print(type(each))
 
 print(f"The total bill with the tip included comes to ${total}, split between {people} people - each person should pay ${each}.")
