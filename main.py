c97f83cf6dfc5a25cee5206597880bf9822f9d58
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-17 20:30:43 -0400

    total

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ percent = int(input("What percentage of the bill would you like to give for the 
 
 people = int(input("How many people are splitting the bill?"))
 
+total = round(bill / (percent / 100 + 1) , 2)
