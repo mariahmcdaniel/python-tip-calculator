13839825889c85294b82b24453dfdce8a004a88e
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-17 20:41:48 -0400

    resolving err

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ print("Welcome to the tip calculator!\n")
 bill = float(input("What is the total price of the bill?\n"))
 
 print(f"bill = {bill}\n")
+print(type(bill))
 
 percent = int(input("What percentage of the bill would you like to give for the tip? 10, 12, or 15\n"))
 
 print(f"percent = {percent}\n")
+print(type(percent))
 
 people = int(input("How many people are splitting the bill?\n"))
 
 print(f"people = {people}\n")
+print(type(people))
 
 total = round(bill / ((percent / 100) + 1) , 2)
 
